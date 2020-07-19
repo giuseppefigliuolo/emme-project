853188ee38aa3fcda9503af3c0a85838f59baa01
giuseppefigliuolo <figliuologiuseppe@gmail.com>
2020-07-19 10:47:13 +0200

    menu fixed

---
+++ js/index.js
@@ -0,0 +1,20 @@
+"use-strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const menuHandler = () => {
+    const menu = document.querySelector(".sidemenu");
+    const menuBtn = document.querySelector(".topbar__menu");
+    const closeMenu = document.querySelector(".sidemenu__close-logo");
+    const menuToggles = [menuBtn, closeMenu];
+
+    // menuToggles.forEach(el => console.log(el));
+
+    menuToggles.forEach(el =>
+      el.addEventListener("click", evt => {
+        !menu.classList.contains("menu__active") ? menu.classList.add("menu__active") : menu.classList.remove("menu__active");
+      })
+    );
+  };
+  menuHandler();
+});
+
+//
