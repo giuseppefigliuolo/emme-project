0b86e54405e71135ae9fbd549a2406ff0c23561e
giuseppefigliuolo <figliuologiuseppe@gmail.com>
2020-08-01 12:47:34 +0200

    gallery started

--- js/index.js
+++ js/index.js
@@ -27,7 +27,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const body = document.querySelector(".landing");
     const sidemenu = document.querySelector(".sidemenu");
 
-    body.style.height = `${window.innerHeight - document.querySelector(".topbar").clientHeight}px`;
+    if (window.location.href.includes("index")) {
+      body.style.height = `${window.innerHeight - document.querySelector(".topbar").clientHeight}px`;
+    }
     sidemenu.style.height = `${window.innerHeight}px`;
   };
 
@@ -35,44 +37,78 @@ document.addEventListener("DOMContentLoaded", () => {
     const topbarLogo = document.querySelector(".topbar__logo");
     const topbarFullLogo = document.querySelector(".topbar p");
     // gallery variables
-    const gallery = document.querySelector(".project__gallery__img-container");
-    const landingDescriptionPos = document.querySelector(".landing__description h2").offsetTop;
-    const videoPos = document.querySelector(".project__video").offsetTop;
-    let lastScroll = landingDescriptionPos;
+    if (window.location.href.includes("index")) {
+      window.addEventListener("scroll", evt => {
+        const gallery = document.querySelector(".project__gallery__img-container");
+        const landingDescriptionPos = document.querySelector(".landing__description h2").offsetTop;
+        const videoPos = document.querySelector(".project__video").offsetTop;
+        let lastScroll = landingDescriptionPos;
+        const movingGallery = (startToScrollPos, EndToScrollPos, galleryToMove) => {
+          if (window.scrollY >= startToScrollPos && window.scrollY < EndToScrollPos) {
+            const newScroll = window.scrollY;
+            const galleryLeftPos = parseInt(getComputedStyle(galleryToMove).left, 10);
+            if (newScroll > lastScroll && galleryLeftPos > -850) {
+              galleryToMove.style.left = `${galleryLeftPos - 3}px`;
+            } else if (newScroll < lastScroll && galleryLeftPos < 5) {
+              galleryToMove.style.left = `${galleryLeftPos + 3}px`;
+            }
+            lastScroll = newScroll;
+          }
+        };
+        movingGallery(landingDescriptionPos, videoPos, gallery);
+        movingGallery(
+          document.getElementById("time-is-now").offsetTop,
+          document.querySelector("#chi-sono").offsetTop,
+          document.querySelector("#trd-project .project__gallery__img-container")
+        );
+      });
+    }
 
     window.addEventListener("scroll", evt => {
-      if (window.scrollY !== 0) {
-        topbarLogo.classList.add("hidden");
-        topbarFullLogo.classList.remove("hidden");
-      } else {
-        topbarLogo.classList.remove("hidden");
-        topbarFullLogo.classList.add("hidden");
-      }
-      // Gallery-scroll handler -> qui va creata una funzione
-      const movingGallery = (startToScrollPos, EndToScrollPos, galleryToMove) => {
-        if (window.scrollY >= startToScrollPos && window.scrollY < EndToScrollPos) {
-          const newScroll = window.scrollY;
-          const galleryLeftPos = parseInt(getComputedStyle(galleryToMove).left, 10);
-          if (newScroll > lastScroll && galleryLeftPos > -850) {
-            galleryToMove.style.left = `${galleryLeftPos - 3}px`;
-          } else if (newScroll < lastScroll && galleryLeftPos < 5) {
-            galleryToMove.style.left = `${galleryLeftPos + 3}px`;
-          }
-          lastScroll = newScroll;
+      const logoSwitch = () => {
+        if (window.scrollY !== 0) {
+          topbarLogo.classList.add("hidden");
+          topbarFullLogo.classList.remove("hidden");
+        } else {
+          topbarLogo.classList.remove("hidden");
+          topbarFullLogo.classList.add("hidden");
         }
       };
-      movingGallery(landingDescriptionPos, videoPos, gallery);
-      movingGallery(
-        document.getElementById("time-is-now").offsetTop,
-        document.querySelector("#chi-sono").offsetTop,
-        document.querySelector("#trd-project .project__gallery__img-container")
-      );
+      logoSwitch();
     });
+  };
+
+  // Gallery: Image-slider
+  const imageSlider = () => {
+    const imgSlider = document.querySelector(".img-slider");
+    const imgs = document.querySelectorAll(".single-project__body__img-container img");
+
+    imgs.forEach(img =>
+      img.addEventListener("click", evt => {
+        imgSlider.classList.toggle("hidden");
+        swipeJs();
+      })
+    );
+
+    let immagini = [];
   };
 
   menuHandler();
   setViewHeight();
   scrollHandler();
+  imageSlider();
+
+  const swipeJs = () => {
+    var mySwiper = new Swiper(".swiper-container", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  };
 });
 
 //
